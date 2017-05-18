38fd13a1ede5db9087624d8b7969028abf72c46e
Yo Vannaravuth <ravuthz@gmail.com>
2017-05-18 14:15:33 +0000

    Apply Post with CrudController and basic route

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,10 +2,66 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\CrudController;
 
-class PostController extends Controller
+class PostController extends CrudController
 {
-    //
+    protected $viewPath = 'admin.posts';
+    protected $modelPath = 'App\Post';
+    protected $entities = ['post', 'posts'];
+    
+    protected $itemPerPage = 10;
+    
+    /**
+     * Set validation rules for both create and update actions 
+     * when them don't set any values
+     */
+    protected $validateRules = [
+        'title' => 'required|unique:posts',
+        'content' => 'required'
+    ];
+    
+    /**
+     * Set validation rule for create action only 
+     */
+    protected $validateCreateRules = [];
+    
+    /**
+     * Set validation rule for update action only
+     */
+    protected $validateUpdateRules = [];
+    
+    /**
+     * Use to generate form inputs on view
+     */
+    protected $formFields = [
+        [
+            'name' => 'title',
+            'type' => 'text',
+            'label' => 'Title: ',
+            'placeholder' => 'Enter the title ...'
+        ],
+        [
+            'name' => 'content',
+            'type' => 'textarea',
+            'label' => 'Content: ',
+            'placeholder' => 'Enter the content here ...'
+        ]
+    ];
+    
+    /**
+     * This function can override to find item 
+     * return $result array();
+     */
+    // public function getSingleRow($id) {
+        
+    // }
+    
+    /**
+     * This function can override to list iten 
+     * return $result array();
+     */
+    // public function getMultipleRows() {
+        
+    // }
 }
