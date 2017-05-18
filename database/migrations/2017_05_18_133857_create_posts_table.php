c9a19ab1cbfe36287ff53190817cbdd2c49291c0
Yo Vannaravuth <ravuthz@gmail.com>
2017-05-18 13:58:42 +0000

    Make model, migration and seeder

--- database/migrations/2017_05_18_133857_create_posts_table.php
+++ database/migrations/2017_05_18_133857_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
             $table->timestamps();
         });
     }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Post;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +14,45 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        
+    
+        Post::insert([
+            [
+                'id' => 1,
+                'title' => 'post 1',
+                'content' => 'post content 1'
+            ],    
+            [
+                'id' => 2,
+                'title' => 'post 2',
+                'content' => 'post content 2'
+            ],
+            [
+                'id' => 3,
+                'title' => 'post 3',
+                'content' => 'post content 3'
+            ],
+            [
+                'id' => 4,
+                'title' => 'post 4',
+                'content' => 'post content 4'
+            ],
+            [
+                'id' => 5,
+                'title' => 'post 5',
+                'content' => 'post content 5'
+            ],
+            [
+                'id' => 6,
+                'title' => 'post 6',
+                'content' => 'post content 6'
+            ],
+            [
+                'id' => 7,
+                'title' => 'post 7',
+                'content' => 'post content 7'
+            ]
+        ]);
+        
     }
 }
